132cb299f1059f029e5367a7cfb2d21c8096a907
Hyunko1 <79427281+UiHyunKang@users.noreply.github.com>
2023-01-02 18:37:15 +0900

    Create beak 1000.java

---
+++ beak 1000.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+		
+		System.out.println(A+B);
+ 
+		in.close();
+	}
+}
+
+
+#이해 1도 안됨 뭐가 뭔지도 모르겠음 
