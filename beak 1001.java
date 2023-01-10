443ce7c33dcce795ddb4b6ec0c8e9182b073d2c2
Hyunko1 <79427281+UiHyunKang@users.noreply.github.com>
2023-01-10 22:41:32 +0900

    Create beak 1001.java

---
+++ beak 1001.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+
+		System.out.println(A-B);
+
+		in.close();
+	}
+}
+
+
+#그러니까... class Main 이라는 함수..? 를 만들고 그안에 아 모르겠다 아직 모르겠는중
