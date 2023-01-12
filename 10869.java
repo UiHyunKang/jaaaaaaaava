fe2d5ab61c9dbfced4c95fdaf8b776261f6d9a32
Hyunko1 <79427281+UiHyunKang@users.noreply.github.com>
2023-01-12 22:44:19 +0900

    Create 10869.java

---
+++ 10869.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+ 
+        in.close();
+ 
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+	}
+}
+
+#구글링 하면서 봐도 모르겠다
+#html을 먼저 하라는데 내일부터 짬내서 배워야겠다
